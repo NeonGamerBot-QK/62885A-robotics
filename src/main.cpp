3e47f1c2599ae003a16ac581797554bdba17c591
Saahil <neon@saahild.com>
2022-11-19 11:37:38 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,65 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       Saahil                                               */
-/*    Created:      11/12/2022, 9:18:51 AM                                    */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------- */
+                                                                            
+// *                          Module:       main.cpp                                                  
+// *                          Author:       Saahil (saahild.com)                                      
+// *                          Created:      11/12/2022, 9:18:51 AM                                    
+// *                          Description:  V5 project                                                
+                                                                            
+/* ----------------------------------------------------------------------------*/
 #pragma region VEXcode Generated Robot Configuration
-// Make sure all required headers are included.
+// * Make sure all required headers are included.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
-
-
+#include <iostream>
 #include "vex.h"
 
 using namespace vex;
 
-// Brain should be defined by default
+//  * Brain should be defined by default
 brain Brain;
 
-
-// START V5 MACROS
-#define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
+// * START V5 MACROS
+#define waitUntil(condition) \
+  do                         \
+  {                          \
+    wait(5, msec);           \
   } while (!(condition))
 
-#define repeat(iterations)                                                     \
+#define repeat(iterations) \
   for (int iterator = 0; iterator < iterations; iterator++)
-// END V5 MACROS
-
+//  END V5 MACROS
 
 // Robot configuration code.
-// 6_1 = red
-motor Motor2 = motor(PORT2, ratio18_1, false);
-motor Motor3 = motor(PORT3, ratio18_1, false);
-motor FlywheelMotor1 = motor(PORT4, ratio36_1, true);
+// 6:1 = blue
+/**
+ * * 6:1 = blue
+ * * 36:1 red
+ * * 18:1 green
+ */
+motor Motor2 = motor(PORT2, ratio6_1, false);
+motor Motor3 = motor(PORT3, ratio6_1, true);
+motor FlywheelMotor1 = motor(PORT4, ratio18_1, true);
 motor FlywheelMotor2 = motor(PORT5, ratio18_1, true);
-motor FlywheelMotor3 = motor(PORT7, ratio18_1, true);
-
-
+motor FlywheelMotor3 = motor(PORT7, ratio6_1, true);
+/**
+ * * Drive train config
+ * @param int wheelTravel
+ * @param int trackWidth
+ */
+const int wheelTravel = 320;
+const int trackWidth = 320;
+const int wheelBase = 130;
+motor_group driveL(Motor2);
+motor_group driveR(Motor3);
+drivetrain myDrivetrain(driveL, driveR, wheelTravel, trackWidth, wheelBase, mm);
 
 #pragma endregion VEXcode Generated Robot Configuration
 
 #include "vex.h"
-// A global instance of competition
+// ! A global instance of competition
 competition Competition;
 controller Controller;
 // The logging system
@@ -65,16 +77,23 @@ controller Controller;
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+void pre_auton(void)
+{
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-
+  // *  Stop all motors
+  FlywheelMotor3.stop();
+  FlywheelMotor1.stop();
+  FlywheelMotor2.stop();
+  Brain.Screen.newLine();
+  Brain.Screen.print("Pre Auton + all motors stopped");
+  myDrivetrain.stop();
 }
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
-/*                              Autonomous Task                              */
+/*                               Autonomous Task                              */
 /*                                                                           */
 /*  This task is used to control your robot during the autonomous phase of   */
 /*  a VEX Competition.                                                       */
@@ -82,11 +101,12 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
+void autonomous(void)
+{
   // ..........................................................................
-  // Insert autonomous user code here.
+  // TODO Insert autonomous user code here.
   // ..........................................................................
-
+  Brain.Screen.print("Autonomous code here.");
 }
 
 /*---------------------------------------------------------------------------*/
@@ -98,73 +118,136 @@ void autonomous(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-int timesPressed = 0;
-void buttonPress(){
-    timesPressed++;//add one to timesPressed
-    // Controller.Screen.clearScreen();
-    FlywheelMotor3.spin(reverse, 60.0, volt);
-    FlywheelMotor1.spin(reverse, 60.0, volt);
-    FlywheelMotor2.spin(forward, 60.0, volt);
-
-    Brain.Screen.print("Pressed A Button %i Times\n", timesPressed);//prints how many times Button A was pressed to the controller's screen
+void buttonPress()
+{
+  // // Controller.Screen.clearScreen();
+  FlywheelMotor3.spin(reverse, 60.0, volt);
+  FlywheelMotor1.spin(forward, 60.0, volt);
+  FlywheelMotor2.spin(reverse, 60.0, volt);
 }
-void stopA(void) {
+void stopA(void)
+{
   FlywheelMotor3.stop();
-FlywheelMotor1.stop();
-FlywheelMotor2.stop();
+  FlywheelMotor1.stop();
+  FlywheelMotor2.stop();
 }
-void testing() {
-    FlywheelMotor3.spin(forward, 60.0, volt);
+void testing()
+{
+  FlywheelMotor3.spin(forward, 60.0, volt);
 }
-void usercontrol(void) {
+void AxisTurn(void)
+{
+  const int axisUD = Controller.Axis2.value();
+  const int axisLR = Controller.Axis1.value();
+  // Brain.Screen.print(axis);
+  if (axisLR == 0)
+  {
+    myDrivetrain.stop();
+  }
+  else if (axisUD == 0)
+  {
+    myDrivetrain.stop();
+  }
+  if (axisUD > 30)
+  {
+    // Forward
+
+    myDrivetrain.drive(forward);
+    Brain.Screen.newLine();
+    Brain.Screen.print("Forward");
+  }
+  else if (axisUD < -30)
+  {
+    // BACKWARDS
+    myDrivetrain.drive(reverse);
+    Brain.Screen.newLine();
+    Brain.Screen.print("Backwards");
+  }
+  else if (axisLR < -30)
+  {
+    // LEFT
+    // //  myDrivetrain.turnFor(left, 1.0, degrees);
+    myDrivetrain.turn(left);
+    Brain.Screen.newLine();
+    Brain.Screen.print("left");
+  }
+  else if (axisLR > 30)
+  {
+    // RIGHT
+    // //  myDrivetrain.turnFor(right, 1.0, degrees);
+    myDrivetrain.turn(right);
+
+    Brain.Screen.newLine();
+    Brain.Screen.print("right");
+  }
+  std::cout << "\nAxis:\n";
+  std::cout << axisUD << " - " << axisLR;
+  std::cout << "\n";
+}
+void MoveForward(void)
+{
+  Motor2.spin(forward, 5.0, volt);
+  Motor3.spin(reverse, 5.0, volt);
+}
+void usercontrol(void)
+{
   // User control code here, inside the loop
-    Controller.ButtonA.pressed(buttonPress);
-    Controller.ButtonA.released(stopA);
-Controller.ButtonB.pressed(testing);
-  while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+  Controller.ButtonR2.pressed(buttonPress);
+  Controller.ButtonR2.released(stopA);
+  Controller.ButtonB.pressed(testing);
+  Controller.Axis2.changed(AxisTurn);
+  Controller.Axis1.changed(AxisTurn);
+  while (1)
+  {
+    // *This is the main execution loop for the user control program.
+    // *Each time through the loop your program should update motor + servo
+    // *values based on feedback from the joysticks.
 
     // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
+    // *Insert user code here. This is where you use the joystick values to
+    // *update your motors, etc.
     // ........................................................................
 
     wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+                    // ! prevent wasted resources.
   }
 }
 
 //
-// Main will set up the competition functions and callbacks.
+//* Main will set up the competition functions and callbacks.
 //
-int main() {
-    //  Brain.Screen.print("Hello");
+int main()
+{
+  //  Brain.Screen.print("Hello");
   // Set up callbacks for autonomous and driver control periods.
 
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-Brain.Screen.print("Hi");
-// Motor2.spin(forward, 60.0, volt);
-Motor2.spin(forward, 5.0, volt);
-Motor3.spin(reverse, 5.0, volt);
-Brain.Screen.print(Motor2.temperature(celsius));
-wait(4000, msec);
-Motor2.stop();
- Motor3.stop();
-// //  Drivetrain.setDriveVelocity(100, percent);
-// //   Drivetrain.setTurnVelocity(20, percent);
+  Brain.Screen.print("Hi");
+  // //Motor2.spin(forward, 60.0, volt);
+  // //Motor2.spin(forward, 5.0, volt);
+  // //Motor3.spin(reverse, 5.0, volt);
+  // //Brain.Screen.print(Motor2.temperature(celsius));
+  // //wait(4000, msec);
 
-// //   Drivetrain.driveFor(reverse, 50, mm);
-// //   Drivetrain.turnFor(left, 45, degrees);
-// //   Drivetrain.driveFor(forward, 3, inches);
-// //   Drivetrain.stop();
-  // Run the pre-autonomous function.
+  // //  Drivetrain.setDriveVelocity(100, percent);
+  // //   Drivetrain.setTurnVelocity(20, percent);
+
+  // //   Drivetrain.driveFor(reverse, 50, mm);
+  // //   Drivetrain.turnFor(left, 45, degrees);
+  // //   Drivetrain.driveFor(forward, 3, inches);
+  // //   Drivetrain.stop();
+  //? Run the pre-autonomous function.
   pre_auton();
 
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
+  // * Prevent main from exiting with an infinite loop.
+  while (true)
+  {
     wait(100, msec);
+  }
+  while (true)
+  {
+    wait(1000, msec);
+    Brain.Screen.clearScreen();
   }
 }
