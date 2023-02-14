e32de13c3ae75f59a0c1d6fc186065d6154b3b0f
Saahil <neon@saahild.com>
2023-02-14 17:26:16 -0500

    Update intake.cpp
    e

--- 2023/src/intake.cpp
+++ 2023/src/intake.cpp
@@ -1,5 +1,5 @@
 
 #include "main.h"
 void IntakeMove(pros::Motor mtr) { mtr = 127; }
-void IntakeReverse(pros::Motor mtr) { mtr = -127; }
+void IntakeReverse(pros::Motor mtr) { mtr = -100; }
 void IntakeStop(pros::Motor mtr) { mtr.brake(); }
